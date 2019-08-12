0d2752d6b0c17ac1d784bfcf0cf184351efec390
Yves Kipondo <yvesdaxmaz@gmail.com>
2019-08-12 03:06:45 +0200

    create app container

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "semantic-ui/dist/semantic.min.css";
 import './index.css';
-import App from './App';
+import ProductList from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ProductList />, document.getElementById('content'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
